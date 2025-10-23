e78f77749c439c76a49e466b51ec7389ef19784e
danipompeudetoledo <danilosantos.itech@gmail.com>
2025-10-22 20:33:17 -0700

    Initial commit - Maven + TestNG project

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import org.testng.Assert;
+public class SampleTest {
+    @Test
+    public void basicTest() {
+        int a = 5;
+        int b = 3;
+        Assert.assertEquals(a + b, 8);
+    }
+}
